14e743527ef14eb175fffab183e17d86244cf16d
Adithya <aprabhakaran@scu.edu>
2014-10-29 01:29:30 -0700

    external js file with all questions. Individual objects have been validated using json lint.

---
+++ questions.js
@@ -0,0 +1,32 @@
+var questions = [					
+		 { 
+			"question": "Which of the following words are included in the definition/derivation of the word ‘Frugal?’", 
+			"choices": [ "Virtuous", "To Enjoy","Cheap", "Of The Essence" ,"A, B, & D" ],
+			"correctChoice": "A, B, & D",
+			"score": 5
+	      },
+	      {
+	      	"question": "In what year is it estimated that the cumulative GDP of emerging markets (excluding China) will surpass that of the developed world?",
+	      	"choices":["2014", "2016", "2022", "2031"],
+	      	"correctChoice": "2016",
+	      	"score": 5
+	      },
+	      {
+	      	"question": "Which of the following is the first step of the Design Innovation Process?",
+	      	"choices":["Define", "Prototype", "Ideate", "Empathize", "Test"],
+	      	"correctChoice": "Empathize",
+	      	"score": 5
+	      },
+	      {
+	      	"question": "What percentage of the world now has access to a mobile phone?",
+	      	"choices":["28%", "46%", "59%", "81%", "97%"],
+	      	"correctChoice": "81%",
+	      	"score": 5
+	      },
+	      {
+	      	"question": "What is ‘reverse innovation’?",
+	      	"choices":["Learning how to design a product by breaking it apart and seeing how it was built.", "Engineering for the emerging market consumer before the developed world consumer.", "An innovation that is developed in an emerging market and is eventually utilized in a mature market."],
+	      	"correctChoice": "An innovation that is developed in an emerging market and is eventually utilized in a mature market.",
+	      	"score": 5
+	      }
+]
