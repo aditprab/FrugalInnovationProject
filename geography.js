75b2f83f6d4de800495fd6952ddecf758b4ecfa2
Adithya <aprabhakaran@scu.edu>
2014-10-31 05:31:56 -0700

    Biggest commit. Almost completely working, but can't get the quiz to show up...

---
+++ geography.js
@@ -0,0 +1,27 @@
+var geography = [					
+		 { 
+			"question": "What island country is immediately to the east of Great Britain?",
+			"choices": [ "Ireland", "India","Kansas", "Greenland"],
+			"correctChoice": "Ireland",
+			"score": 5
+	      },
+	      {
+	      	"question": "What country is immediately to the north of the United States?",
+	      	"choices":["North United States", "Canada", "Mexico", "Chile"],
+	      	"correctChoice": "Canada",
+	      	"score": 5
+	  
+	      },
+	      {
+	      	"question": "Which city is farthest from San Jose?",
+	      	"choices":["Los Angeles", "Sacramento", "San Francisco", "San Diego"],
+	      	"correctChoice": "San Diego",
+	      	"score": 5
+	      },
+	      {
+	      	"question": "What African country borders Egypt to the east?",
+	      	"choices":["Zamibia", "Somalia", "Libya", "Morrocco"],
+	      	"correctChoice": "",
+	      	"score": 5
+	      }
+]
