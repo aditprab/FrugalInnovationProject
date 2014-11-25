b75f6deddaf75c452f17723a33c8cee2f2c7b40a
Nathan Lam <nathan.tlam@gmail.com>
2014-11-24 20:24:58 -0800

    final...for now

--- deliverable2/blog.php
+++ deliverable2/blog.php
@@ -1,11 +1,32 @@
 <?php
+/*Checking whether a user navigated to the php page. If visiting the page, not sending a post request, nothing should be entered into the database.*/
+if(!isset($_POST['author']) && !isset($_POST['title']) && !isset($_POST['post']) && !isset($_POST['tag']))
+{
+    echo("Error");
+    return;
+}
 
+$tableName = "blog";
 $author = "Anonymous";
-if(isset($_POST["author"]))
-    $author = $_POST["author"];
+if(isset($_POST['author']) && !empty($_POST['author']))
+    $author = $_POST['author'];
 $title = $_POST['title'];
 $post = $_POST['post'];
 $date = date("m/d/Y");
+$tags = "";
+if(isset($_POST['tag']) && !empty($_POST['tag']))
+{
+    $array = $_POST['tag'];
+
+    $pattern = "/#/";
+    $tagsArray = explode(" ", $array);
+    foreach($tagsArray as $value)
+    {
+        if(!preg_match($pattern, $value))
+            $tags .= "#";
+        $tags .= $value . " ";
+    }
+}
 
 $con = mysql_connect("dbserver.engr.scu.edu","nlam","00000965060");
 if (!$con)
@@ -15,22 +36,15 @@ if (!$con)
 
 mysql_select_db("sdb_nlam", $con);
 
-$tags = "";
-if(isset($_POST['tag']))
+$result = mysql_query("SELECT COUNT(*) FROM $tableName");
+
+while($resultArray = mysql_fetch_array($result))
 {
-    $array = $_POST['tag'];
-
-    $pattern = "/#/";
-    $tagsArray = explode(" ", $array);
-    foreach($tagsArray as $value)
-    {
-            if(!preg_match($pattern, $value))
-                    $tags .= "#";
-            $tags .= $value . " ";
-    }
+    $postNumber = $resultArray['COUNT(*)'];
 }
+$postNumber++;
 
-$sql = "INSERT INTO blogEntries VALUES ('$author','$title','$post', '$date', '$tags')";
+$sql = "INSERT INTO $tableName VALUES ('$postNumber', '$author','$title','$post', '$date', '$tags')";
 
 if (!mysql_query($sql,$con))
 {
---
+++ deliverable2/submitProposal.php
@@ -0,0 +1,38 @@
+<?php
+
+if(!isset($_POST['projectName']) || !isset($_POST['projectDescription']) || !isset($_POST['members']))
+{
+    echo("Error");
+    return;
+}
+
+$tableName = "proposals";
+$projectName = $_POST['projectName'];
+$projectDescription = $_POST['projectDescription'];
+$teamMembers = $_POST['members'];
+
+$con = mysql_connect("dbserver.engr.scu.edu","nlam","00000965060");
+if (!$con)
+{
+    die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("sdb_nlam", $con);
+
+$result = mysql_query("SELECT COUNT(*) FROM $tableName");
+
+while($resultArray = mysql_fetch_array($result))
+{
+    $proposalNumber = $resultArray['COUNT(*)'];
+}
+$proposalNumber++;
+
+$sql = "INSERT INTO $tableName VALUES ('$proposalNumber', '$projectName','$projectDescription','$teamMembers')";
+
+if (!mysql_query($sql,$con))
+{
+    die('Error: ' . mysql_error());
+}
+
+mysql_close($con)
+?>
